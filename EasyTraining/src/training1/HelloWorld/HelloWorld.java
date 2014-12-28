fd190df58c1fab5e492d293ea4ab1db67f543e4f
yamabe009 <yamabe.kinoko@gmail.com>
2014-12-29 00:14:12 +0900

    [add]Add Project.

---
+++ EasyTraining/src/training1/HelloWorld/HelloWorld.java
@@ -0,0 +1,9 @@
+package training1.HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+
+}
---
+++ EasyTraining/src/training1/HelloWorld/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Output "Hello World" to console only.
+ * First step of JavaTraining.
+ * @author yamabe009
+ */
+package training1.HelloWorld;
